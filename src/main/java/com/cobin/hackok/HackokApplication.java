16f25894185213a5ff6097def6617b2eb0184a23
alenjb <alenjb@naver.com>
2024-01-23 02:19:55 +0900

    프로젝트 생성

---
+++ src/main/java/com/cobin/hackok/HackokApplication.java
@@ -0,0 +1,13 @@
+package com.cobin.hackok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cobin/hackok/HackokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobin.hackok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

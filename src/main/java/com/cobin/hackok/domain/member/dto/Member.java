de7cecf99a254c3f471fe5f09b07034655d2f753
alenjb <alenjb@naver.com>
2024-01-24 11:05:39 +0900

    feat: Member에서 사용할 DTO 생성, Member 등급 클래스 생성
    - 사용자에 관한 정보 DTO를 생성함
    - 사용자 등급 클래스 생성

---
+++ src/main/java/com/cobin/hackok/domain/member/dto/Member.java
@@ -0,0 +1,16 @@
+package com.cobin.hackok.domain.member.dto;
+
+import com.cobin.hackok.domain.member.domain.MemberGrade;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;            //아이디
+    @NotBlank private String loginId;     //로그인 아이디
+    @NotBlank private String name;        //이름
+    @NotBlank private String password;    //비밀번호
+    private String birthDay;    //생년월일
+    private String joinDay;     //가입 날짜
+    private MemberGrade memberGrade;   //회원등급
+}

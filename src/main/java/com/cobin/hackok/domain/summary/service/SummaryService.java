dea16fe30ca77db6ce21074d4a57384e9a02b763
alenjb <alenjb@naver.com>
2024-02-10 00:42:37 +0900

    feat(summary/service):chat-gpt API 사용 기능 구현
    - chat-gpt API를 사용하는 기능들을 구현함

--- src/main/java/com/cobin/hackok/domain/summary/service/SummaryService.java
+++ src/main/java/com/cobin/hackok/domain/summary/service/SummaryService.java
@@ -1,18 +1,22 @@
 package com.cobin.hackok.domain.summary.service;
 
-import com.cobin.hackok.domain.global.config.IdConfig;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-
 import java.util.Map;
 
 public interface SummaryService {
-    // 네이버 api 요약 기능
+    /**
+     *
+     * 네이버 api 요약 기능
+     */
     public Map<String, Object> getSummary(com.cobin.hackok.domain.summary.dto.RequestBody requestBody);
 
-    // open ai api 키워드 추츨 기능
+
+    /**
+     *     openAI api 기능
+     */
+    // chat gpt에게 요청
+    Map<String, Object> getKeywords(String content);
+    //키워드들을 맵 형태로 변환
+    Map<String, Object> mapKeywords(String keywords);
+
 
 }
--- src/main/java/com/cobin/hackok/domain/summary/service/SummaryServiceImpl.java
+++ src/main/java/com/cobin/hackok/domain/summary/service/SummaryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cobin.hackok.domain.summary.service;
 
-import com.cobin.hackok.domain.global.config.IdConfig;
+import io.github.flashvayne.chatgpt.service.ChatgptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -8,19 +9,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.HashMap;
 import java.util.Map;
-
+@Slf4j
 @Service
 public class SummaryServiceImpl implements SummaryService{
+
+    private final ChatgptService chatgptService;
+    private final ApiProperties properties;
+    final String requestMent = "다음 나오는 내용의 핵심단어 5개를 뽑아서 콤마로 구분해줘.\n";
+
+    public SummaryServiceImpl(ChatgptService chatgptService, ApiProperties properties) {
+        this.chatgptService = chatgptService;
+        this.properties = properties;
+    }
+
     // 네이버 api 요약 기능
     public Map<String, Object> getSummary(com.cobin.hackok.domain.summary.dto.RequestBody requestBody) {
         String apiUrl = "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize";
-        IdConfig idConfig = new IdConfig();
 
         WebClient webClient = WebClient.builder().baseUrl(apiUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader("X-NCP-APIGW-API-KEY-ID", idConfig.getClientId())
-                .defaultHeader("X-NCP-APIGW-API-KEY", idConfig.getClientSecret())
+                .defaultHeader("X-NCP-APIGW-API-KEY-ID", properties.getClientId())
+                .defaultHeader("X-NCP-APIGW-API-KEY", properties.getClientSecret())
                 .build();
 
         // POST 요청을 보내고 응답을 받아옴
@@ -30,5 +41,35 @@ public class SummaryServiceImpl implements SummaryService{
                 .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
                 .block();
         return response;
+    }
+
+    @Override
+    public Map<String, Object> getKeywords(String content) {
+        // 키워드를 뽑는 멘트
+        content = requestMent + content;
+
+        // ChatGPT 에게 질문 후 매핑해서 리턴
+        return mapKeywords(chatgptService.sendMessage(content));
+    }
+
+    @Override
+    public Map<String, Object> mapKeywords(String keywords) {
+        log.info("키워드" + keywords);
+        String[] splitKeywords = keywords.split(",");
+        for(String k:splitKeywords){
+            log.info("나눈키워드" + k);
+        }
+        HashMap<String, Object> keywordMap = new HashMap<>();
+        keywordMap.put("keyword1", removeQuotes(splitKeywords[0]));
+        keywordMap.put("keyword2", removeQuotes(splitKeywords[1]));
+        keywordMap.put("keyword3", removeQuotes(splitKeywords[2]));
+        keywordMap.put("keyword4", removeQuotes(splitKeywords[3]));
+        keywordMap.put("keyword5", removeQuotes(splitKeywords[4]));
+        return keywordMap;
+    }
+
+    // 따옴표 제거
+    private String removeQuotes(String keyword) {
+        return keyword.replaceAll("^\"|\"$", "");
     }
 }

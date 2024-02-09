dc6711392e95792815a7293aac417e72683b989b
alenjb <alenjb@naver.com>
2024-02-09 19:55:34 +0900

    feat(global):네이버 API 계정 정보 등록

---
+++ src/main/java/com/cobin/hackok/domain/global/config/IdConfig.java
@@ -0,0 +1,10 @@
+package com.cobin.hackok.domain.global.config;
+
+import lombok.Getter;
+
+@Getter
+public class IdConfig {
+    String clientId = ""; //(X-NCP-APIGW-API-KEY-ID)
+    String clientSecret = "";   //(X-NCP-APIGW-API-KEY)
+
+}

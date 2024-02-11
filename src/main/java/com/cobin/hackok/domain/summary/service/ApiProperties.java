bd13b002cf9d9fb4c60a06a62112ea919c9caf9b
alenjb <alenjb@naver.com>
2024-02-11 23:49:52 +0900

    feat(summary):matgim API 정보 필드 생성

--- src/main/java/com/cobin/hackok/domain/summary/service/ApiProperties.java
+++ src/main/java/com/cobin/hackok/domain/summary/service/ApiProperties.java
@@ -6,10 +6,18 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "naver")
+@ConfigurationProperties(prefix = "api")
 @Getter @Setter
 public class ApiProperties {
-
+    /**
+     * 네이버 API 정보
+     */
     private String clientId;
     private String clientSecret;
+    /**
+     * matgim API 정보
+     */
+    private String matigimApiKey;
+
+
 }
